7db0bea83d8885d06ce1b6b9c66bcb81e14db9c0
Yerzhanov <dearhauer@gmail.com>
2023-02-21 19:12:24 +0600

    Обработаны файлы

---
+++ products/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Product, ProductCategory
+
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'category', 'price', 'image']
+
+class CategoryForm(ModelForm):
+    class Meta:
+        model = ProductCategory
+        fields = ['name', 'description']
