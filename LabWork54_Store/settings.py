276a78b3c8b5e989d27a298c47e1d930a6cd4e2c
Yerzhanov <dearhauer@gmail.com>
2023-03-13 13:43:07 +0600

    Добавлено и зарегистрировано новое приложение

--- LabWork54_Store/settings.py
+++ LabWork54_Store/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'products',
     'users',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
